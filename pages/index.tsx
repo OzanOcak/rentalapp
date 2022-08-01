f8dfb7ca507e353a19d5e026cbbde900f755994b
OzanOcak <ozanocak@gmail.com>
2022-08-01 13:12:12 -0400

    layout & nav

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,11 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Layout from "../components/layout";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return (
-    <div className="flex justify-center items-center h-screen font-bold text-xl text-[2rem] text-[green] ">
-      {" "}
-      hello world!
-    </div>
-  );
+  return <Layout main={<div>home</div>} />;
 };
 
 export default Home;
